65de5f5fb61d21495652837699c30b0864e7247a
ianlogeais <71567996+ianlogeais@users.noreply.github.com>
2021-10-18 20:02:42 +0200

    Update Class.cs

--- Class.cs
+++ Class.cs
@@ -1,20 +1,40 @@
 using System;
 
-namespace Cours2
+namespace Cours3
 {
-    class test
+    class Joueur
     {
+        String name;
+        int score;
+        int choix;
+        bool ia;
 
-        static void Main(string[] args) 
-        {
-            
+        public Joueur(String name, bool ia){
+            this.name = name;
+            this.score = 0;
+            this.choix = 0;
+            this.ia = ia;
         }
 
+        public String Name{
+            get { return name; }
+            set { name = value; }
+        }
 
+        public int Score{
+            get { return score; }
+            set { score = value; }
+        }
 
+        public int Choix{
+            get { return choix; }
+            set { choix = value; }
+        }
+
+        public bool IA{
+            get { return ia; }
+            set { ia = value; }
+        }
 
     }
-
-
-
 }
